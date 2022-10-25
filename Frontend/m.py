519302cec6a00ce1352af462690a2681fffa9c26
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-10-25 08:20:14 +0530

    update

--- Frontend/m.py
+++ Frontend/m.py
@@ -14,7 +14,19 @@ from code.DiseaseModel import DiseaseModel
 from code.helper import prepare_symptoms_array
 import seaborn as sns
 import matplotlib.pyplot as plt
+# import hydralit_components as hc
 
+# menu_data = [
+#     {'label':"Left End"},
+#     {'label':"Book"},
+#     {'label':"Component"},
+#     {'label':"Dashboard"},
+#     {'label':"Right End"},
+# ]
+
+# menu_id = hc.nav_bar(menu_definition=menu_data)
+
+# st.info(f"{menu_id=}")
 
 # loading the models
 diabetes_model = pickle.load(open("../models/diabetes_model.sav", "rb"))
